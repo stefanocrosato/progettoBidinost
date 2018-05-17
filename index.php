63bbc7f89f91493950354a468a24400e6a089c24
DESKTOP-S9F731J\crosa <stefano.crosato@bearzi.org>
2018-05-17 10:01:08 +0200

    + collega il php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: crosa
+ * Date: 17/05/2018
+ * Time: 09:32
+ */
+
+
+    // Make a MySQL Connection
+/*
+    mysql_connect("localhost", "root", "EscxiPass_123") or die(mysql_error());
+    mysql_select_db("prova") or die(mysql_error());
+*/
+    //echo "ci sei riuscito";
+function writeMsg() {
+    echo "Hello world!";
+}
+
+writeMsg(); // call the function
+
+prova ad usare ajax
