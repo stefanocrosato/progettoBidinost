11b9d615b7376c80c5cb324c73959f38fb76a71c
DESKTOP-S9F731J\crosa <stefano.crosato@bearzi.org>
2018-06-06 17:59:06 +0200

    aggiunta marquee

--- from.php
+++ from.php
@@ -15,9 +15,28 @@ error_reporting(E_ALL | E_STRICT);
 </head>
 <body>
 <div id="all">
+    <font color="white">
+    <marquee
+            color:white
+            loop="-1"
+            scrollamount="1"
+            scrolldelay="20"
+            direction="up"
+            height="150"
+            width="200"
+            align="right">
+        Sito creato da <br>
+        Crosato Stefano e<br>
+        Micol Massimiliano<br>
+        Benvenuti sul vostro sito di prenotazioni!<br>
+        Se volete inserire dati sul database:<br>
+        index.html<br>
+        Grazie di usare il nostro servizio<br>
+        <br><br><br><br>
+    </marquee>
     <div id="form">
         <h1 class="display-1" style="color:white">LuftWaffer</h1>
-        <p class="font-weight-light" style="color:white">Benvenuti sul vostro sito di prenotazioni!</p>
+
         <form action="receive.php">
             <select class="selectpicker" name="partenza">
                 <option>Seleziona la tua partenza</option>
@@ -30,7 +49,7 @@ error_reporting(E_ALL | E_STRICT);
                 ?>
             </select>
             <select name="arrivo">
-                <option>Seleziona la tua partenza</option>
+                <option>Seleziona la tua destinazione</option>
                 <?php
                 $items = ConnectionDB::getFromSQL("SELECT * FROM viaggio;");
                 var_dump($items);
